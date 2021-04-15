08af9d6ca5ea993dd9eee2b58005a1d2ac429c43
Ryfyguy <ryfyguy@hotmail.com>
2021-04-15 13:38:41 -0400

    Chapter 2 part 4&5 complete

--- Become a Spring Developer/learning-spring/src/main/java/com/rsf/spring/learningspring/LearningSpringApplication.java
+++ Become a Spring Developer/learning-spring/src/main/java/com/rsf/spring/learningspring/LearningSpringApplication.java
@@ -1,6 +1,10 @@
 package com.rsf.spring.learningspring;
 
+import com.rsf.spring.learningspring.data.entity.Guest;
+import com.rsf.spring.learningspring.data.entity.Reservation;
 import com.rsf.spring.learningspring.data.entity.Room;
+import com.rsf.spring.learningspring.data.repository.GuestRepository;
+import com.rsf.spring.learningspring.data.repository.ReservationRepository;
 import com.rsf.spring.learningspring.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -25,6 +29,30 @@ public class LearningSpringApplication {
 		@GetMapping
 		public Iterable<Room> getRooms(){
 			return this.roomRepository.findAll();
+		}
+	}
+
+	@RestController
+	@RequestMapping("/guests")
+	public class GuestController{
+		@Autowired
+		private GuestRepository guestRepository;
+
+		@GetMapping
+		public Iterable<Guest> getGuests(){
+			return this.guestRepository.findAll();
+		}
+	}
+
+	@RestController
+	@RequestMapping("/reservations")
+	public class ReservationController{
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@GetMapping
+		public Iterable<Reservation> getReservations(){
+			return this.reservationRepository.findAll();
 		}
 	}
 
---
+++ Become a Spring Developer/learning-spring/src/main/java/com/rsf/spring/learningspring/data/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.rsf.spring.learningspring.data.repository;
+
+import com.rsf.spring.learningspring.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+}
