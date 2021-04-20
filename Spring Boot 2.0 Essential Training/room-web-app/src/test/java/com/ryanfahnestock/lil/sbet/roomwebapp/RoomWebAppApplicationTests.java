5952098946e7e03493307275c3d6068a140247e2
Ryfyguy <ryfyguy@hotmail.com>
2021-04-20 11:05:19 -0400

    Spring Boot 2.0 Essential Training: Chapter 4 part 3 Completed

---
+++ Spring Boot 2.0 Essential Training/room-web-app/src/test/java/com/ryanfahnestock/lil/sbet/roomwebapp/RoomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanfahnestock.lil.sbet.roomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
