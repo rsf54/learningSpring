46831f08ca6c4357c5fd7d8a4616fe7fa6ce029e
Ryfyguy <ryfyguy@hotmail.com>
2021-04-20 10:47:20 -0400

    Spring Boot 2.0 Essential Training: Chapter 5 part 6 Completed

--- Spring Boot 2.0 Essential Training/room-web-app/src/main/java/com/ryanfahnestock/lil/sbet/roomwebapp/config/SecurityConfig.java
+++ Spring Boot 2.0 Essential Training/room-web-app/src/main/java/com/ryanfahnestock/lil/sbet/roomwebapp/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/","/api/*").permitAll().anyRequest().authenticated()
+        http.authorizeRequests().antMatchers("/","/api/*", "/api/rest/*").permitAll().anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
                 .and().logout().permitAll();
     }
