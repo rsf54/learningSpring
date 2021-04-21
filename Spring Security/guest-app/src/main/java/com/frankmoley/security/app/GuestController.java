fbdbf595481b8d4b7b3c72f15454946c77a20fe7
Ryfyguy <ryfyguy@hotmail.com>
2021-04-21 11:37:22 -0400

    Spring: Spring Security Chapter 2 part 7 Complete

--- Spring Security/guest-app/src/main/java/com/frankmoley/security/app/GuestController.java
+++ Spring Security/guest-app/src/main/java/com/frankmoley/security/app/GuestController.java
@@ -35,6 +35,11 @@ public class GuestController {
         return "index";
     }
 
+    @GetMapping(value="/login")
+    public String getLoginPage(Model model){
+        return "login";
+    }
+
     @GetMapping(value="/guests")
     @PreAuthorize("hasRole('ROLE_USER')")
     public String getGuests(Model model){
