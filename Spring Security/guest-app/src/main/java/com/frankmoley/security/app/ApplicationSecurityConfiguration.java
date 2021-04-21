35fc291dcf3ed88c57843deae8bc69909119a0ec
Ryfyguy <ryfyguy@hotmail.com>
2021-04-21 11:28:26 -0400

    Spring: Spring Security Chapter 2 part 5 Complete

--- Spring Security/guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ Spring Security/guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -6,22 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -32,7 +28,16 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
+        provider.setAuthoritiesMapper(authoritiesMapper());
         return provider;
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper authoritiesMapper(){
+        SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setConvertToUpperCase(true);
+        authorityMapper.setDefaultAuthority("USER");
+        return authorityMapper;
     }
 
     @Override
--- Spring Security/guest-app/src/main/java/com/frankmoley/security/app/auth/LandonUserDetailsService.java
+++ Spring Security/guest-app/src/main/java/com/frankmoley/security/app/auth/LandonUserDetailsService.java
@@ -5,14 +5,18 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LandonUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final AuthGroupRepository  authGroupRepository;
 
-    public LandonUserDetailsService(UserRepository userRepository) {
+    public LandonUserDetailsService(UserRepository userRepository, AuthGroupRepository authGroupRepository) {
         super();
         this.userRepository = userRepository;
+        this.authGroupRepository = authGroupRepository;
     }
 
     @Override
@@ -21,6 +25,7 @@ public class LandonUserDetailsService implements UserDetailsService {
         if(null==user){
             throw new UsernameNotFoundException("cannot find username: " + username);
         }
-        return new LandonUserPrincipal(user);
+        List<AuthGroup> authGroups = this.authGroupRepository.findByUsername(username);
+        return new LandonUserPrincipal(user, authGroups);
     }
 }
