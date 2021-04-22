3dae346ca3de4d0160b45531ea261f20e1c60a5c
Ryfyguy <ryfyguy@hotmail.com>
2021-04-22 11:56:45 -0400

    Creating Your First Spring Boot Microservice: Chapter 3 part 3 Completed

--- Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourRepository.java
+++ Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourRepository.java
@@ -3,5 +3,8 @@ package com.ryanfah.ec.explorecali.repo;
 import com.ryanfah.ec.explorecali.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(String code);
 }
