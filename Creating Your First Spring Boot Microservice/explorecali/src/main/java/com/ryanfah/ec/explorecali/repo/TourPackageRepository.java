df79b25f834e37883a7f90386a5afef614aec38c
Ryfyguy <ryfyguy@hotmail.com>
2021-04-22 12:08:13 -0400

    Creating Your First Spring Boot Microservice: Chapter 3 part 5 Completed

--- Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourPackageRepository.java
+++ Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourPackageRepository.java
@@ -2,9 +2,12 @@ package com.ryanfah.ec.explorecali.repo;
 
 import com.ryanfah.ec.explorecali.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     /*
     Signature<return type> findBy"EntityAttributeName(camelcase)"(Parameter w/ Datatype of EAN)
@@ -27,6 +30,29 @@ public interface TourPackageRepository extends CrudRepository<TourPackage, Strin
      -- is the same as --
     List<Tour> findByTourPackageCodeAndDifficultyAndRegionAndPriceLessThan(String code, Difficulty difficulty, Region region, Integer maxPrice;
     */
-
     Optional<TourPackage> findByName(String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourRepository.java
+++ Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourRepository.java
@@ -3,6 +3,7 @@ package com.ryanfah.ec.explorecali.repo;
 import com.ryanfah.ec.explorecali.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -18,4 +19,29 @@ sort - category, type (asc or desc), defaults by id
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
     List<Tour> findByTourPackageCode(String code);
     //Page<Tour> findByTourPackageCode(String code, Pageable pageable); --Makes method pageable
+
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
