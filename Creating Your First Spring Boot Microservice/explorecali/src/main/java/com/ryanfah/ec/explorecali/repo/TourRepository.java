6aa75e608c0c98a26fd6a0e5b7833976d0ecd95c
Ryfyguy <ryfyguy@hotmail.com>
2021-04-22 12:01:56 -0400

    Creating Your First Spring Boot Microservice: Chapter 3 part 4 Completed

--- Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourRepository.java
+++ Creating Your First Spring Boot Microservice/explorecali/src/main/java/com/ryanfah/ec/explorecali/repo/TourRepository.java
@@ -2,9 +2,20 @@ package com.ryanfah.ec.explorecali.repo;
 
 import com.ryanfah.ec.explorecali.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+/*
+URL sort codes:
+size - num of elements, defaults 20
+page - which page, defaults 0
+sort - category, type (asc or desc), defaults by id
+.../tours?size=3&page=1&sort=title,asc
+(by method) .../tours/search/findByTourPackageCode?code=CC&size=2&sort=title,asc
+ */
+
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
     List<Tour> findByTourPackageCode(String code);
+    //Page<Tour> findByTourPackageCode(String code, Pageable pageable); --Makes method pageable
 }
