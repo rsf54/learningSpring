66975e77fc8c89f0f385e2abba87ad5da38ae2a8
Ryfyguy <ryfyguy@hotmail.com>
2021-04-25 15:54:23 -0400

    Spring: Spring MVC Chapter 1 part 10 complete

---
+++ Spring MVC/hplusapp/src/main/java/com/test/hplus/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.test.hplus.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String goHome(){
+        System.out.println("In home controller");
+        return "index"; //name of the view to go to
+    }
+}
